31163d561b57be46eec34dc352922ed754b9655c
silascastro <silascastro15@gmail.com>
2019-10-07 14:05:37 -0400

    stilizando login

--- appPedido/index.js
+++ appPedido/index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import Home from './src/screens/Home';
+import Login from './src/screens/Login';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Home);
+AppRegistry.registerComponent(appName, () => Login);
--- appPedido/src/screens/Login.js
+++ appPedido/src/screens/Login.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput, Button, Statusbar} from 'react-native';
 
 
-export default class Login extends Component<Props> {
+export default class Login extends Component {
   render() {
     return (
+      
       <View style={styles.container}>
+        <View style={styles.loginCard}>
+          <TextInput placeholder="Digite aqui o seu email"  underlineColorAndroid="#0000ff" />
+          <TextInput  underlineColorAndroid='#0000ff' textContentType='password' placeholder="Senha" secureTextEntry={true}/>
+          <View style={styles.lembrarSenha}>
+            <Text >Lembrar minha senha</Text>
+          </View>
+        </View>
+        <View style={styles.btn}>
+          <Button title="login" color="#124d34" />
+        </View>
       </View>
     );
   }
@@ -14,9 +25,25 @@ export default class Login extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    //justifyContent: 'center',
+    //alignItems: 'center',
+    //backgroundColor: '#F5FCFF',
+    backgroundColor: '#3ca597',
+  },
+  loginCard:{
+    //height: 40,
+    marginTop: 50,
+    marginBottom: 10, 
+    marginLeft: 20, 
+    marginRight: 20, 
+    backgroundColor: '#ffffff'
+  },
+  lembrarSenha: {
+    height: 30
+  },
+  btn:{
+    marginLeft: 20, 
+    marginRight: 20,
   },
   welcome: {
     fontSize: 20,
