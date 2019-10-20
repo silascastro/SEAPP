4bd28502f0480f8cd15e5346064482ad40f41f45
silascastro <silascastro15@gmail.com>
2019-10-20 14:54:09 -0400

    atualizando rotas

--- api/src/routes/cliente.js
+++ api/src/routes/cliente.js
@@ -5,7 +5,7 @@ const clienteController = require('../controllers/clienteController');
 router.get('/',clienteController.get);
 router.get('/byid/:id',clienteController.getOnClienteById);
 router.get('/notcont/:nome', clienteController.getOneClienteByNameHasNotCont);
-router.get('/:nome',clienteController.getOneClienteByName);
+router.get('/byname/:nome',clienteController.getOneClienteByName);
 
 
 module.exports = router;
