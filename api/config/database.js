62ffe34fdc9fb5f4044ba4364110edcbfd2c4f1a
silascastro <silascastro15@gmail.com>
2020-04-16 10:22:51 -0400

    mudando host

--- api/config/database.js
+++ api/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   username: 'postgres',
   password: 'masterkey',
   database: 'estoque_db',
-  host: '200.146.28.55',
+  host: 'localhost',
   port: '5432',
   dialect: 'postgres',
   ssl: false
